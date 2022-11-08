d3baac52787f4d0cad90e461ce088b8ca7171033
Ian Santana <thief.jones@gmail.com>
2022-11-08 13:44:20 +0000

    buton_añadido

--- index_capas.js
+++ index_capas.js
@@ -1,28 +1,18 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let hamburgerButton = document.querySelector('.hamburgerMenu');
+let menu = document.querySelector('#menu_interior');
+let isShowed = false;
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+
+showOrHideMenu = () =>{
+    if(!isShowed){
+        menu.classList.add('showed');
+        isShowed = true;
+    } else{
+        menu.classList.remove('showed');
+        isShowed = false;
+    }
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+window.onload = () => {
+    hamburgerButton.addEventListener('click', showOrHideMenu);
 }
